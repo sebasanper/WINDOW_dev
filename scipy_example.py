d31d280d9ff84a3d6462aa89f513538902a5470d
sebasanper <s.sanchezperezmoreno@tudelft.nl>
2017-12-03 22:58:57 +0100

    save

--- scipy_example.py
+++ scipy_example.py
@@ -14,12 +14,12 @@ prob.model = LCOE()
 prob.setup()
 
 def objfunc(x):
-    # prob['indep2.downwind_spacing'] = x[0]
-    # prob['indep2.crosswind_spacing'] = x[1]
-    # prob['indep2.odd_row_shift_spacing'] = x[2]
-    # prob['indep2.layout_angle'] = x[3]
-    layout = [[x[i], x[74 + i]] for i in range(74)]
-    prob['indep2.layout'] = layout
+    prob['indep2.downwind_spacing'] = x[0]
+    prob['indep2.crosswind_spacing'] = x[1]
+    prob['indep2.odd_row_shift_spacing'] = x[2]
+    prob['indep2.layout_angle'] = x[3]
+    # layout = [[x[i], x[74 + i]] for i in range(74)]
+    # prob['indep2.layout'] = layout
     prob.run_model()
     f = prob['analysis.lcoe'][0]
     g = [0.0] * 2
@@ -28,8 +28,8 @@ def objfunc(x):
 
     return f
 
-# bounds = [(570.0, 2500.0), (570.0, 2500.0), (0.0, 1250.0), (0.0, 180.0)]
-bounds = [(484000.0, 504000.0) for _ in range(74)] + [(5.715e6, 5.74e6) for _ in range(74)]
+bounds = [(570.0, 2500.0), (570.0, 2500.0), (0.0, 1250.0), (0.0, 180.0)]
+# bounds = [(484000.0, 504000.0) for _ in range(74)] + [(5.715e6, 5.74e6) for _ in range(74)]
 result = differential_evolution(objfunc, bounds, popsize=1)
 # initial_guess = [1000.0, 1400.0, 0.0, 30.0]
 # minimizer_kwargs = {"method":"Nelder-Mead"}
