4db46d41fbdb4690e048a7b001a0bbff130f7b85
Sebastian <s.sanchezperezmoreno@tudelft.nl>
2017-12-03 17:10:22 -0700

    pyswarm irregular

--- pyswarm_example.py
+++ pyswarm_example.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 from openmdao.api import Problem
 from workflow_cheap import LCOE
 import time
+from farm_description import NT
 
 prob = Problem()
 # print clock(), "Before defining model"
@@ -16,30 +17,29 @@ prob.setup()
 from pyswarm import pso
 
 def objfunc(x):
-    prob['indep2.downwind_spacing'] = x[0]
-    prob['indep2.crosswind_spacing'] = x[1]
-    prob['indep2.odd_row_shift_spacing'] = x[2]
-    prob['indep2.layout_angle'] = x[3]
-    # layout = [[x[i], x[74 + i]] for i in range(74)]
-    # prob['indep2.layout'] = layout
+    layout = [[x[i], x[74 + i]] for i in range(74)]
+    prob['indep2.layout'] = layout
     prob.run_model()
     f = prob['analysis.lcoe'][0]
+    print f
     # g = [0.0] * 2
     # g[0] = prob['constraint_boundary.magnitude_violations'][0]
     # g[1] = prob['constraint_distance.magnitude_violations'][0]
 
     return f
 
-# def con1(x):	
-#     g[0] = - prob['constraint_boundary.magnitude_violations'][0]
-#     # g[1] = prob['constraint_distance.magnitude_violations'][0]
-#     x1 = x[0]
-#     x2 = x[1]
-#     return [-(x1 + 0.25)**2 + 0.75*x2]
+def con1(x):	
+    layout = [[x[i], x[74 + i]] for i in range(74)]
+    prob['indep2.layout'] = layout
+    prob.run_model()
+    g1 = - prob['constraint_boundary.magnitude_violations'][0]
+    g2 = - prob['constraint_distance.magnitude_violations'][0]
+    # g[1] = prob['constraint_distance.magnitude_violations'][0]
+    return [g1, g2]
 
-lb = [570.0, 570.0, 0.0, 0.0]
-ub = [2500.0, 2500.0, 1250.0, 180.0]
+lb = [484000.0 for _ in range(NT)] + [5.715e6 for _ in range(NT)]
+ub = [504000.0 for _ in range(NT)] + [5.74e6 for _ in range(NT)]
 
-xopt, fopt = pso(objfunc, lb, ub, f_ieqcons=None)
+xopt, fopt = pso(objfunc, lb, ub, f_ieqcons=con1)
 print xopt
 print fopt
